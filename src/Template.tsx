a83431dcd1cc1f4d810654fed906da235a8a6260
kkimsuhyeon <wzxy2002@naver.com>
2020-09-10 20:41:13 +0900

    topbar -ing

--- src/Template.tsx
+++ src/Template.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from 'view/Home';
+import { Topbar } from 'view/App';
 import { PrivateRoute } from 'component/PrivateRoute';
 
 const Template = () => {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Redirect to="/" />
-    </Switch>
+    <>
+      <Topbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/test" component={() => { return <div>test</div>; }} />
+        <Route path="/test2" component={() => { return <div>test2</div>; }} />
+        <Route path="/test3" component={() => { return <div>test3</div>; }} />
+        <Redirect to="/" />
+      </Switch>
+    </>
   );
 };
 
--- src/view/App/Topbar.tsx
+++ src/view/App/Topbar.tsx
@@ -1,3 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Div, Flex } from 'lib/style/styled';
 
 import logo from 'assets/logo2.png';
+
+const Wrapper = styled.div`
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 999;
+  background-color: #1e3269;
+  height: 100px;
+`;
+
+const Test = styled(Flex)`
+  position: relative;
+`;
+
+const Img = styled.div`
+  background-image: url(${logo});
+  background-repeat: no-repeat;
+  background-size: contain;
+  width: 100px;
+  height: 100px;
+`;
+
+const Topbar = () => {
+  return (
+    <Wrapper>
+      <Test>
+        <Img />
+      </Test>
+    </Wrapper>
+  );
+};
+
+export default Topbar;
